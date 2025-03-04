e2a58726ebc9d2d397f0b6e8d0b83e8aaeb8229d
Jonathan <jonathancombsjdc@gmail.com>
2025-03-04 17:52:40 -0600

    Testing CORS requests for web browser functionality

--- LiveTelemetry/scripts/telemetryDisplay.js
+++ LiveTelemetry/scripts/telemetryDisplay.js
@@ -1,6 +1,34 @@
 
-
 var socket = io("http://localhost:3000");
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, socket, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+var xhr = createCORSRequest('GET', socket);
+if (!xhr) {
+  throw new Error('CORS not supported');
+}
 
 //Variable Definitions
 let map;
